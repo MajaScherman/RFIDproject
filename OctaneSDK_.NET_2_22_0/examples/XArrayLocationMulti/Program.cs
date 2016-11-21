afe8cab3081e9fd2ea811e18444bbf5d80a912f8
lab <you@example.com>
2016-11-21 15:52:07 +0900

    Add initial project files

---
+++ OctaneSDK_.NET_2_22_0/examples/XArrayLocationMulti/Program.cs
@@ -0,0 +1,228 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+//    Multiple xArray Location
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using Impinj.OctaneSdk;
+using System.Collections.Generic;
+
+namespace OctaneSdkExamples
+{
+    class Program
+    {
+
+        // This example does a weighted average with just 2 xArrays.  You can additional xArrays by adding xArray elements to the
+        // 2 xArrays defined in xArrays[] below:
+
+        //                                       Reader      HeightCM, FacXcm, FacYcm, Orient, Session
+        XArray[] xArrays = {   new XArray("xarray-XX-XX-XX",   300,       0,       0,      0,     2),      
+                               new XArray("xarray-XX-XX-XX",   300,       0,     400,      0,     3) };
+
+        ReaderMode READER_MODE = ReaderMode.AutoSetDenseReaderDeepScan;   // Recommended moded for xArray
+        const int COMPUTE_WINDOW_SEC = 30;
+
+        // Use dictionaries to store Confidence, WeightedX and WeightedY and Cycle Lengths.
+        Dictionary<string, double> CycleLengths = new Dictionary<string, double>();
+        Dictionary<string, TagReadInfo> TagReadInfo = new Dictionary<string, TagReadInfo>();
+
+        public Program()
+        {
+            ImpinjReader[] readers = new ImpinjReader[xArrays.Length];
+            for (int i = 0; i < readers.Length; i++)
+            {
+                readers[i] = new ImpinjReader();
+                LaunchXArray(readers[i], xArrays[i]);
+            }
+            // Wait for the user to press enter.
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+            for (int i = 0; i < xArrays.Length; i++)
+            {
+                CloseXArray(readers[i]);
+            }
+        }
+        public void LaunchXArray(ImpinjReader reader, XArray xArray)
+        {
+            try
+            {
+                // Connect to the reader.
+                // Change the ReaderHostname constant in SolutionConstants.cs 
+                // to the IP address or hostname of your reader.
+                reader.Connect(xArray.Hostname);
+
+                // Assign the LocationReported event handler.
+                // This specifies which method to call
+                // when a location report is available.
+                reader.LocationReported += OnLocationReported;
+                // Don't forget to define diagnostic method
+                reader.DiagnosticsReported += OnDiagnosticsReported;
+
+                // Apply the newly modified settings.
+                reader.ApplySettings(GetPrepareSettings(reader, xArray));
+
+                // Start the reader
+                reader.Start();
+            }
+            catch (OctaneSdkException e)
+            {
+                // Handle Octane SDK errors.
+                Console.WriteLine("Octane SDK exception: {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                // Handle other .NET errors.
+                Console.WriteLine("Exception : {0}", e.Message);
+            }
+        }
+        public Settings GetPrepareSettings(ImpinjReader reader, XArray xArray)
+        {
+            // Get the default settings
+            // We'll use these as a starting point
+            // and then modify the settings we're 
+            // interested in.
+            Settings settings = reader.QueryDefaultSettings();
+
+            // Put the xArray into location Role
+            settings.XArray.Mode = XArrayMode.Location;
+
+            // Enable all three report types
+            settings.XArray.Location.EntryReportEnabled = true;
+            settings.XArray.Location.UpdateReportEnabled = true;
+            settings.XArray.Location.ExitReportEnabled = true;
+            // Enable Diagnostic reports here, soon to be deprecated
+            settings.XArray.Location.DiagnosticReportEnabled = true;
+
+            // Set xArray placement parameters
+            // The mounting height of the xArray, in centimeters
+            settings.XArray.Placement.HeightCm = xArray.Height;
+            settings.XArray.Placement.FacilityXLocationCm = xArray.FacilityXcm;
+            settings.XArray.Placement.FacilityYLocationCm = xArray.FacilityYcm;
+            settings.XArray.Placement.OrientationDegrees = xArray.Orientation;
+
+            // Compute Window and Gen2 Settings
+            settings.XArray.Location.ComputeWindowSeconds = COMPUTE_WINDOW_SEC;
+            settings.ReaderMode = READER_MODE;
+            settings.Session = xArray.Session;
+            settings.XArray.Location.TagAgeIntervalSeconds = 60;
+
+            // Specify how often we want to receive location reports
+            settings.XArray.Location.UpdateIntervalSeconds = 10;
+
+            // Exercise: Filter out extraneous tags
+        
+            settings.Filters.TagFilter1.TagMask = "9999";
+            settings.Filters.TagFilter1.MemoryBank = MemoryBank.Epc;
+            settings.Filters.TagFilter1.BitPointer = BitPointers.Epc;
+            settings.Filters.TagFilter1.BitCount = 16;
+            settings.Filters.Mode = TagFilterMode.OnlyFilter1;  // Use just one filter
+         
+            return settings;
+        }
+        // This event handler will be called when a location report is ready.
+        void OnLocationReported(ImpinjReader reader, LocationReport report)
+        {
+            string EpcStr = report.Epc.ToHexString();
+
+            // Compute confidence. Make sure that the first cycle report came in before computing the Weighted averages.
+            if (!CycleLengths.ContainsKey(reader.Address) || CycleLengths[reader.Address] == 0)
+                return;
+
+            // If first time
+            if (!TagReadInfo.ContainsKey(EpcStr))
+                TagReadInfo.Add(EpcStr, new TagReadInfo());
+
+            double Mult = Math.Floor((double)COMPUTE_WINDOW_SEC * 1000000 / CycleLengths[reader.Address]);  
+            if (Mult == 0) Mult=1;
+            double Confidence = report.ConfidenceFactors.ReadCount / Mult;
+            Console.WriteLine(reader.Address + "  " + EpcStr + " x=" + report.LocationXCm + " y=" + report.LocationYCm + " conf=" + Confidence);
+            // Weighted X
+            double WgtX = Confidence * report.LocationXCm;
+            TagReadInfo[EpcStr].WeightedX += WgtX;
+
+            // Weighted Y
+            double WgtY = Confidence * report.LocationYCm;
+            TagReadInfo[EpcStr].WeightedY += WgtY;
+            
+            // TagReadCounts 
+            TagReadInfo[EpcStr].Confidence += Confidence;
+
+            // Pick a reader to key off the Averaging calculation
+            // Let's use the last one.
+            if (reader.Address.Equals(xArrays[xArrays.Length - 1].Hostname))
+            {
+                Console.Write("Weighted: "+EpcStr);
+                if (TagReadInfo[EpcStr].Confidence != 0)
+                {
+                    Console.Write(" x=" + Math.Floor(TagReadInfo[EpcStr].WeightedX / TagReadInfo[EpcStr].Confidence));
+                    Console.WriteLine(" y=" + Math.Floor(TagReadInfo[EpcStr].WeightedY / TagReadInfo[EpcStr].Confidence));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Read. Confidence is 0");
+                }
+                // Reinitialize variables
+                TagReadInfo[EpcStr].WeightedX = 0;
+                TagReadInfo[EpcStr].WeightedY = 0;
+                TagReadInfo[EpcStr].Confidence = 0;
+            }
+            
+        }
+        private void OnDiagnosticsReported(ImpinjReader reader, DiagnosticReport report)
+        {
+            uint[] reportMetricsList = report.Metrics.ToArray();
+            // Warning!!! Accessing diagnostic codes will not be supported in future releases.
+            if (reportMetricsList[0] == 100)  // End of Cycle
+            {
+                Console.WriteLine("xArray=" + reader.Address+"  CycleTime=" + (int)reportMetricsList[1]/1000+"ms");
+                // Store latest Cycle time
+                if (CycleLengths.ContainsKey(reader.Address))
+                    CycleLengths[reader.Address] = (int)reportMetricsList[1];
+                else
+                    CycleLengths.Add(reader.Address, (int)reportMetricsList[1]);
+            }
+        }
+        private void CloseXArray(ImpinjReader reader)
+        {
+            // Apply the default settings before exiting.
+            reader.ApplyDefaultSettings();
+
+            // Disconnect from the reader.
+            reader.Disconnect();
+        }
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+    }
+    class XArray 
+    {
+        public XArray(String Hostname, ushort Height, int FacilityXcm, int FacilityYcm, short Orientation, ushort Session)
+        {
+            this.Hostname = Hostname;
+            this.Height = Height;
+            this.FacilityXcm = FacilityXcm;
+            this.FacilityYcm = FacilityYcm;
+            this.Session = Session;
+        }
+        public string Hostname   { get; set; }
+        public ushort Height     { get; set; }
+        public int FacilityXcm   { get; set; }
+        public int FacilityYcm   { get; set; }
+        public short Orientation { get; set; }
+        public ushort Session    { get; set; }
+    }
+    class TagReadInfo
+    {
+        public TagReadInfo()
+        {
+            this.Confidence = 0;
+            this.WeightedX = 0;
+            this.WeightedY = 0;
+        }
+        public double Confidence { get; set; }
+        public double WeightedX { get; set; }
+        public double WeightedY { get; set; }
+    }
+}
---
+++ OctaneSDK_.NET_2_22_0/examples/XArrayLocationWam/Program.cs
@@ -0,0 +1,228 @@
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+//    xArray Location and Wam Example
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using System;
+using Impinj.OctaneSdk;
+using System.Collections.Generic;
+
+namespace OctaneSdkExamples
+{
+    class Program
+    {
+        // Create an instance of the ImpinjReader class.
+        static ImpinjReader reader = new ImpinjReader();
+        // Shared between Both Roles
+        ReaderMode READER_MODE = ReaderMode.AutoSetDenseReaderDeepScan;     // Recommended moded for xArray
+        
+        // WAM Role Stuff
+        const SearchMode WAM_SEARCH_MODE         = SearchMode.SingleTarget;
+        const int WAM_SESSION                    = 3;                          // With just one xArray could use either session 2 or 3
+        // If just inventorying a few hundreds Monza tags you can use single target with suppression especially
+        // when using a fast reader mode like ReaderMode.AutoSetDenseReaderDeepScan.
+        // Use the next 2 commented out lines to define SingleTarget TagFocus.
+     /* const SearchMode WAM_SEARCH_MODE         = SearchMode.TagFocus;  
+        const int WAM_SESSION                    = 1; 
+     */
+
+        const int TAG_POPULATION_ESTIMATE       = 2;     // Use a small estimate when using a combination of 52 xArray beams and tag suppression.
+                                                         // Typically the beam is only finding a small subset of the total number of tags.
+        // Location Role Stuff
+        const int XARRAY_HEIGHT                = 280;    // This is for a low ceiling
+        const int COMPUTE_WINDOW               = 10;     // Fairly short compute window
+        const int TAG_AGE_INTERVAL             = 60;
+        const int UPDATE_INTERVAL              = 10;     // How often you receive updates in seconds
+        const ushort LOCATION_SESSION          = 2;      // When using multiple xArrays use a combination of Session 2 and 3
+
+        // Run Timing parameters. 
+        // Note: Increase duration for larger tag populations
+        const int WAM_ROLE_DURATION            = 30 * 1000;                   // Time in milliseconds to run WAM
+        const int LOCATION_ROLE_DURATION       = COMPUTE_WINDOW *1000 +500;   // Get enough time to run one compute window
+        const int INTERATIONS                  = 2;                           // WAM and Location Roles Interations
+        const int SESSION_2_OR_3_PERSISTENCE = 120 * 1000;                    // If Using WAM Session 2 or 3 wait for tags to decay before restarting WAM Role
+                                                                              // MR6 has long decay time so wait at least 2 minutes
+                                                                              // Your tags may vary
+        // Collect tags read and their counts per inventory round
+        Dictionary<string, Tag>            WamTags = new Dictionary<string, Tag>();
+        Dictionary<string, LocationReport> LocTags = new Dictionary<string, LocationReport>();
+
+        public Program()
+        {
+            try
+            {
+                reader.Connect(SolutionConstants.ReaderHostname);
+
+                Console.WriteLine("WAM with Location: " + SolutionConstants.ReaderHostname);
+
+                for (int i = 0; i < INTERATIONS; i++)
+                {
+                    // WAM Role
+                    Console.WriteLine("Running WAM. Please wait " + WAM_ROLE_DURATION / 1000 + " Sec." + " Session=" + WAM_SESSION + " Target=" + WAM_SEARCH_MODE);
+                    SetupWamMode();
+                    System.Threading.Thread.Sleep(WAM_ROLE_DURATION);
+                    ShutdownWamMode();
+                    Console.WriteLine("WAM Results:  TagsRead=" + WamTags.Count);
+                    foreach (var item in WamTags)
+                    {
+                        Tag tag = item.Value;
+                        Console.WriteLine(item.Key + "  Ant=" + tag.AntennaPortNumber+ "\tRSSI=" + tag.PeakRssiInDbm);
+                    }
+                    Console.WriteLine();
+                    WamTags.Clear();
+
+                    // Location Role
+                    Console.WriteLine("Running Location. Please wait " + LOCATION_ROLE_DURATION / 1000 + " sec.");
+                    SetupLocationMode();
+                    System.Threading.Thread.Sleep(LOCATION_ROLE_DURATION);
+                    ShutdownLocationMode();
+                    Console.WriteLine("Location Results: " + LocTags.Count +" Tags Read");
+                    foreach (var item in LocTags)
+                    {
+                        LocationReport tag = item.Value;
+                        Console.WriteLine(item.Key + "\tReadCount=" + tag.ConfidenceFactors.ReadCount + "\tX=" + tag.LocationXCm + "\tY=" + tag.LocationYCm);
+                    }
+                    LocTags.Clear();
+                    Console.WriteLine();
+                    // Wait for tag percistance to complete before starting WAM again
+                    if ((i < INTERATIONS - 1) && (WAM_SESSION == 2 || WAM_SESSION == 3)) {
+                        Console.WriteLine("Wait " + SESSION_2_OR_3_PERSISTENCE/1000 + " Sec. for tag percistance to complete before starting WAM again");
+                        System.Threading.Thread.Sleep(SESSION_2_OR_3_PERSISTENCE);
+                    }
+                }
+                // Apply the default settings before exiting.
+                reader.ApplyDefaultSettings();
+                // Disconnect from the reader.
+                reader.Disconnect();
+            }
+            catch (OctaneSdkException e)
+            {
+                // Handle Octane SDK errors.
+                Console.WriteLine("Octane SDK exception: {0}", e.Message);
+            }
+            catch (Exception e)
+            {
+                // Handle other .NET errors.
+                Console.WriteLine("Exception : {0}", e.Message);
+            }
+        }
+        // WAM code
+        public void SetupWamMode()
+        {
+            Settings settings = reader.QueryDefaultSettings();
+            // Put the xArray into location mode
+            settings.XArray.Mode = XArrayMode.WideAreaMonitor;
+
+            settings.Report.IncludeAntennaPortNumber = true;
+            settings.Report.IncludePeakRssi = true;
+            // Set the reader mode, search mode and session
+            settings.ReaderMode = READER_MODE;
+            settings.SearchMode = WAM_SEARCH_MODE;
+            settings.Session = WAM_SESSION;
+            settings.TagPopulationEstimate = TAG_POPULATION_ESTIMATE;
+
+            // Enable all Antennas (Beams)
+            settings.Antennas.EnableAll();
+            // Gen2 Filtering
+            settings = SetupFilter(settings);
+
+            reader.ApplySettings(settings);
+            reader.TagsReported += OnTagsReported;
+
+            // Start reading.
+            reader.Start();
+        }
+        public void ShutdownWamMode()
+        {
+            reader.Stop();
+            reader.TagsReported -= OnTagsReported;
+        }
+        // Location code
+        public void SetupLocationMode()
+        {
+            // Add Locations Report delegate
+            reader.LocationReported += OnLocationReported;
+            // Start with defaults
+            Settings settings = reader.QueryDefaultSettings();
+            // Put the xArray into location mode
+            settings.XArray.Mode = XArrayMode.Location;
+
+            // Enable all three report types
+            settings.XArray.Location.EntryReportEnabled = true;
+            settings.XArray.Location.UpdateReportEnabled = true;
+            settings.XArray.Location.ExitReportEnabled = true;
+
+            // The HeightCm of the xArray, in centimeters
+            settings.XArray.Placement.HeightCm = XARRAY_HEIGHT;
+            settings.XArray.Placement.FacilityXLocationCm = 0;
+            settings.XArray.Placement.FacilityYLocationCm = 0;
+            settings.XArray.Placement.OrientationDegrees = 0;
+
+            // Motion Window and Tag age
+            settings.XArray.Location.ComputeWindowSeconds = COMPUTE_WINDOW;
+            settings.ReaderMode = READER_MODE;
+            settings.Session = LOCATION_SESSION;
+            settings.XArray.Location.TagAgeIntervalSeconds = TAG_AGE_INTERVAL;
+            settings.XArray.Location.UpdateIntervalSeconds = UPDATE_INTERVAL;
+
+            // Gen2 Filtering
+            settings = SetupFilter(settings);
+
+            // Apply and start Reading in Location mode
+            reader.ApplySettings(settings);
+            reader.Start();
+        }
+        public void ShutdownLocationMode()
+        {
+            reader.Stop();
+            // Remove location report delegate
+            reader.LocationReported += OnLocationReported;
+        }
+
+        // Main Program
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        // Location Reports
+        void OnLocationReported(ImpinjReader reader, LocationReport report)
+        {
+            string EpcStr = report.Epc.ToHexString();
+            // Collect tags read from the last location report
+            if (LocTags.ContainsKey(EpcStr))
+                LocTags.Remove(EpcStr);
+            LocTags.Add(EpcStr, report);
+            // Comment out next line to see L every time a tag is reported
+            // Console.Write("L");
+        }
+        // WAM reports
+        void OnTagsReported(ImpinjReader sender, TagReport report)
+        {
+            foreach (Tag tag in report)
+            {
+                string EpcStr = tag.Epc.ToHexString();
+                // Collect tags read and their counts per inventory round
+                if (WamTags.ContainsKey(EpcStr))
+                    WamTags.Remove(EpcStr);
+                WamTags.Add(EpcStr, tag);
+                // Comment out next line to see W every time a tag is reported
+                // Console.Write("W");
+            }
+        }
+        Settings SetupFilter(Settings settings)
+        {
+            
+        /*  // Filter out extranious tags
+            settings.Filters.TagFilter1.TagMask = "9999";  // Only match tags with EPCs with this prefix
+            settings.Filters.TagFilter1.BitCount = 16; 
+            settings.Filters.TagFilter1.MemoryBank = MemoryBank.Epc;
+            settings.Filters.TagFilter1.BitPointer = BitPointers.Epc;
+            settings.Filters.TagFilter1.BitCount = 16;  
+            settings.Filters.Mode = TagFilterMode.OnlyFilter1;  // Use just one filter
+        */    
+            return settings;
+        }
+    }
+}
